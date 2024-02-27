220414e2d73dec056bdb0f9b782fd3c1015a21a2
Darkexpeller <wcultivate@gamil.com>
2024-02-28 02:36:25 +0800

    去除调试信息

--- src/HttpHandle.cpp
+++ src/HttpHandle.cpp
@@ -1,6 +1,7 @@
 #include "HttpHandle.h"
 #include <sys/socket.h>
 #include "is_error.h"
+using namespace std;
 int HttpHandle::HttpSend(std::string mes, int _FD)
 {
     if (HttpHandle::HttpMes(mes, 0).IsHttpMessage())
@@ -14,24 +15,185 @@ int HttpHandle::HttpSend(std::string mes, int _FD)
     }
 }
 
-int HttpHandle::HttpMes::HttpParseURL(HttpHandle::HttpMes &mes, HttpHandle::Url& content) // 这个函数应该在需要的时候再调用，而不是对每一个http都调用
+HttpHandle::HttpMes::HttpMes(string mes, int _FD) : message(mes), socket_fd(_FD),is_http{false}
+{
+	HttpParseHeader();
+}
+void HttpHandle::HttpMes::Extractcontent(string::iterator it)
+{
+	while (it != message.end())
+	{
+		content += *it;
+	}
+}
+void HttpHandle::HttpMes::HttpParseHeader()
+{
+	string Line_temp;
+	string::iterator it = message.begin();
+	while (it != message.end() || *(Header.begin() + Header.size() - 1) != "\r\n")
+	{
+		if (*it != '\n')
+		{
+			Line_temp += *it; // Adds the *it to the string
+		}
+		else
+		{
+			Line_temp += *it;
+			Header.push_back(Line_temp); // Adds the Line_temp to the vector
+			Line_temp.clear();			 // clear temp to parse next line
+		}
+		it++;
+	}
+	Header.pop_back(); // delete the blank line
+	vector<string>::iterator pHeaderLine = Header.begin();
+	string::iterator pFirstLine = (*pHeaderLine).begin();
+
+	// Handle the first line RequestType, ProtocolType, ProtocolVersion
+
+	string RequestType, URL,End, ProtocolType, ProtocolVersion ;
+	// The "End" variable is declared here to read ProtocolType and ProtocolVersion at once
+	string *OperatorList[] = {&RequestType, &URL, &End, &ProtocolType, &ProtocolVersion}; // An array of Pointers is declared to simplify the code
+	string **pOperator = OperatorList;
+	std::cout << "FirstLine=" << *pHeaderLine << std::endl;
+	// If you can ensure the HTTP message's format is ture,some condition can be ignored to simplify code
+	while (pFirstLine != pHeaderLine->end() && *pFirstLine != '\r' && *pOperator != OperatorList[3])
+	{
+		std::cout << "*pFirstLine=" << *pFirstLine << std::endl;
+		if (*pFirstLine ==' ')
+		{
+			pFirstLine ++;
+			pOperator++;
+			continue;
+		}
+		else{
+			**pOperator += *(pFirstLine++);
+		}
+		
+	}
+	std::cout << "End=" << End << std::endl;
+	pOperator++;
+	for (string::iterator i = End.begin(); i != End.end(); i++)
+	{
+		//std::cout << "*i=" << *i << std::endl;
+		//std::cout << "**pOperator=" << **pOperator << std::endl;
+		if (*i == '/')
+		{
+			pOperator++;
+		}
+		else{
+			**pOperator += *i;
+		}
+		
+	}
+	HttpMap["RequestType"] = RequestType;
+	HttpMap["URL"] = URL;
+	HttpMap["ProtocolType"] = ProtocolType;
+	HttpMap["ProtocolVersion"] = ProtocolVersion;
+	if(ProtocolType=="HTTP")          // 如果不是http报文，则return函数，且讲is_http设置为false;
+	{
+		is_http = true;
+	}
+	else{
+		is_http = false;
+		return; 
+	}
+	// Handle the header to the map
+	pHeaderLine++;
+	string key, value;
+	size_t position = 0;
+
+	while (pHeaderLine != Header.end())
+	{
+		position = pHeaderLine->find_first_of(':');
+
+		// if found
+		if (position != pHeaderLine->npos)
+		{
+			for (int i = 0; i < position; i++)
+			{
+				key += (*pHeaderLine)[i];
+			}
+			position += 2;
+			for (; position < pHeaderLine->length() && (*pHeaderLine)[position] != '\r'; position++)
+			{
+				value += (*pHeaderLine)[position];
+			}
+			HttpMap[key] = value;
+			key.clear();
+			value.clear();
+			pHeaderLine++;
+		}
+
+		else // not found':'
+		{
+			pHeaderLine++;
+		}
+	}
+}
+string HttpHandle::HttpMes::GetHeadersValue(string key)
+{
+	string value;
+	if (key == "")  
+	{
+		vector<string>::iterator ThisLine = Header.begin();
+		while (ThisLine != Header.end())
+		{
+			value += *ThisLine;
+			ThisLine++;
+		}
+	}
+	else
+	{
+		map<string, string>::iterator iter = HttpMap.find(key);
+		if (iter == HttpMap.end())
+		{
+			std::cout<<"Not "<<key<<" in headers";
+			value = "";
+		}
+		else if(iter->second=="")
+		{
+			std::cout << "key " << key << " is blank";
+			value = "";
+		}
+		else if(iter->second!="")
+		{
+			value = iter->second;
+		}
+	}
+	return value;
+}
+
+
+
+int HttpHandle::HttpMes::HttpParseURL(Url &content) // 这个函数应该在需要的时候再调用，而不是对每一个http都调用111
                                                                                // 以免造成资源浪费
                                                                                // 去除协议，域名，端口后的path
                                                                                // 这个方法比较全面，但操作偏多，适合健全性需求较强场景
                                                                                // 对于可控的http通讯大可简化，使用另外一个版本
 {
-    std::string URL = mes.GetHeadersValue("URL");
-    std::string Scheme, DomainName, Port, Path, Parm, Anchor;
-    std::string *OperatorList[] = {&Scheme, &DomainName, &Port, &Path, &Parm, &Anchor};
+
+	if(IsHaveParsedURL ==true)
+	{
+		content = URLstruct;
+		return 1;
+	}
+	IsHaveParsedURL = true;
+    std::string URL =GetHeadersValue("URL");
+	if(URL=="")
+	{
+		return 0;
+	}
+    // std::string Scheme, DomainName, Port, Path, Parm, Anchor;
+    // std::string *OperatorList[] = {&Scheme, &DomainName, &Port, &Path, &Parm, &Anchor};
     std::string *pOperator = &content.Scheme;
-    std::size_t SchemeFlagPosition = URL.find_first_of("://");
-    std::size_t ParmFlagPosition = URL.find_first_of("?");
-    std::size_t AnchorFlagPosition = URL.find_first_of("#");
+    std::size_t SchemeFlagPosition = URL.find("://");
+    std::size_t ParmFlagPosition = URL.find("?");
+    std::size_t AnchorFlagPosition = URL.find("#");   //注意不要用find_first_of
 
     std::size_t BeginPosition = 0;
     std::size_t preEndposition = 0;
     std::size_t Endposition = SchemeFlagPosition; // 找到是否有协议域;
-                                                  // 会不会其他地方出现://？？？
+                                                  // 会不会其他地方出现://？？？                                              
     if (Endposition == URL.npos)
     {
         Endposition = 0;
@@ -39,7 +201,6 @@ int HttpHandle::HttpMes::HttpParseURL(HttpHandle::HttpMes &mes, HttpHandle::Url&
     }
     else
     {
-
         for (int i = 0; i < Endposition; i++)
         {
             *pOperator += URL[i];
@@ -47,14 +208,15 @@ int HttpHandle::HttpMes::HttpParseURL(HttpHandle::HttpMes &mes, HttpHandle::Url&
         BeginPosition = Endposition + 3; // 改变搜索的开始位置;
         pOperator++;                     // 更改操作的字符串;
     }
-
+    
     // 接下来一定是域名
     preEndposition = Endposition;
     Endposition = URL.find_first_of('/', BeginPosition);
     if (Endposition == BeginPosition) // 这里等于BeginPosition主要是因为有可能没有域名，直接就是从根目录开始(往往伴随的是连协议都没有)
     {
-        pOperator++;
+		pOperator+=2;
         Endposition = preEndposition;
+
     }
     else
     { // 有域名的
@@ -76,17 +238,22 @@ int HttpHandle::HttpMes::HttpParseURL(HttpHandle::HttpMes &mes, HttpHandle::Url&
     }
     // 在这里处理path
     // Endposition = URL.find_first_of('?',BeginPosition);
-    for (int i = BeginPosition; i < URL.size(); i++)
+    for (std::string::iterator i = URL.begin()+BeginPosition; i != URL.end(); ++i)
     {
-        if (URL[i] == '?' || URL[i] == '#')
+        if (*i == '?' || *i == '#')
         {
-            BeginPosition = i;
-            pOperator++;
+			BeginPosition = i - URL.begin();
+			pOperator++;
             break;
         }
         else
         {
-            *pOperator += URL[i];
+            *pOperator += *i;
+        }
+		if(i==URL.end()-1)   //只有路径没有其他部分的情况
+		{
+            URLstruct = content;
+            return 1;
         }
     }
     if (BeginPosition == ParmFlagPosition) // 遇到了?
@@ -103,7 +270,8 @@ int HttpHandle::HttpMes::HttpParseURL(HttpHandle::HttpMes &mes, HttpHandle::Url&
                 {
                     *pOperator += URL[i];
                 }
-                return 1; //解析完锚点后解析结束
+                URLstruct = content;
+				return 1; //解析完锚点后解析结束
             }
             else{
                 *pOperator += URL[i];
@@ -123,8 +291,13 @@ int HttpHandle::HttpMes::HttpParseURL(HttpHandle::HttpMes &mes, HttpHandle::Url&
                 *pOperator += URL[i];
             }
         }
+        URLstruct = content;
         return 1; //解析完锚点后解析结束
     }
 
     //
+}
+bool HttpHandle::HttpMes::IsHttpMessage()
+{
+    return is_http;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-﻿#include"CreateTcpSocket.h"
-#include"HttpHandle.h"
-#include"is_error.h"
-#include<time.h>
-#include<filesystem>
-//192.168.219.128:9005
+﻿#include "CreateTcpSocket.h"
+#include "HttpHandle.h"
+#include "is_error.h"
+#include <time.h>
+#include <filesystem>
+// 192.168.219.128:9005
 using std::cout;
 using std::endl;
 
@@ -11,54 +11,63 @@ int main()
 {
 
 	clock_t start, finish;
-	const char* filename = "1.txt";
-	FILE* fo = fopen(filename, "w+");
-	int sockfd = CreateServerTcpSocket(9005); //in_port
+	const char *filename = "1.txt";
+	FILE *fo = fopen(filename, "w+");
+	int sockfd = CreateServerTcpSocket(9005); // in_port
 	int opt = 1;
-	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void*)&opt, sizeof(opt));
-	//接受客户端
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&opt, sizeof(opt));
+	// 接受客户端
 	struct sockaddr_in client_addr;
 	socklen_t client_addr_len = sizeof(client_addr);
 	bzero(&client_addr, sizeof(client_addr));
 	printf("begin to accept\n");
-	int client_sockfd = accept(sockfd, (sockaddr*)&client_addr, &client_addr_len);
-	//int client_sockfd = accept(sockfd, NULL, NULL);
-	
-	char buffer[1024];
-	bzero(buffer, sizeof(buffer));
-	printf("有接入\n");
-	
-	recv(client_sockfd, buffer, sizeof(buffer), 0);
-	fprintf(fo, buffer);
-	printf("%s", buffer);
-	//shutdown(sockfd, 2); 
-	start = clock();
-	finish = clock();
-	cout << "seocnd= " <<(double) (finish - start) / CLOCKS_PER_SEC << "s " << endl;
-	HttpHandle::HttpMes http(buffer, sockfd);
-	std::string value = http.GetHeadersValue("Host");
-	cout << "URL=" << http.GetHeadersValue("URL")<<endl;
-	cout << "RequestType=" << http.GetHeadersValue("RequestType")<<endl;
-	cout << "ProtocolType=" << http.GetHeadersValue("ProtocolType")<<endl;
-	cout << "ProtocolVersion=" << http.GetHeadersValue("ProtocolVersion")<<endl;
+	while (true)
+	{
+		int client_sockfd = accept(sockfd, (sockaddr *)&client_addr, &client_addr_len);
+		// int client_sockfd = accept(sockfd, NULL, NULL);
 
-	cout<<value<<endl;
-	cout << "length of value is" << value.length() << endl;
-	HttpHandle::Url urls;
-	try
-	{
-		http.HttpParseURL(urls);
-	}
-	catch(const std::exception& e)
-	{
-		cout << "xieyi=" << urls.Scheme << endl;
+		char buffer[1024];
+		bzero(buffer, sizeof(buffer));
+		printf("有接入\n");
+
+		recv(client_sockfd, buffer, sizeof(buffer), 0);
+		fprintf(fo, buffer);
+		printf("%s", buffer);
+		// shutdown(sockfd, 2);
+		start = clock();
+		finish = clock();
+		cout << "seocnd= " << (double)(finish - start) / CLOCKS_PER_SEC << "s " << endl;
+		HttpHandle::HttpMes http(buffer, sockfd);
+		std::string value = http.GetHeadersValue("Host");
+		cout << "URL=" << http.GetHeadersValue("URL") << endl;
+		cout << "RequestType=" << http.GetHeadersValue("RequestType") << endl;
+		cout << "ProtocolType=" << http.GetHeadersValue("ProtocolType") << endl;
+		cout << "ProtocolVersion=" << http.GetHeadersValue("ProtocolVersion") << endl;
+
+		cout << value << endl;
+		cout << "length of value is" << value.length() << endl;
+		HttpHandle::Url urls;
+
+		try
+		{
+			http.HttpParseURL(urls);
+		}
+		catch (const std::exception &e)
+		{
+			cout << "Scheme=" << urls.Scheme << endl;
+			cout << "Domain=" << urls.DomainName << endl;
+			std::cerr << e.what() << '\n';
+		}
+
+		cout << "Scheme=" << urls.Scheme << endl;
 		cout << "Domain=" << urls.DomainName << endl;
-		std::cerr << e.what() << '\n';
+		cout << "Port=" << urls.Port << endl;
+		cout << "Path=" << urls.Path << endl;
+
+		cout << "Parm=" << urls.Parm << endl;
+		shutdown(sockfd, SHUT_WR); // 立即并同时关闭sockfd上的读和写
+		// close(sockfd);//引用数-1
 	}
-	
-	cout << "xieyi=" << urls.Scheme << endl;
-	cout << "Domain=" << urls.DomainName << endl;
-	shutdown(sockfd, SHUT_WR);//立即并同时关闭sockfd上的读和写
-	//close(sockfd);//引用数-1
+
 	return 1;
 }
